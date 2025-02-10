ab70c0c53d3f23d9daece4b37880b91e27e32b32
andrey-blight <kizhinov05@gmail.com>
2025-02-10 17:07:11 +0300

    Add report

---
+++ practice/practise-5/src/main/java/hse/kpo/report/ReportBuilder.java
@@ -0,0 +1,32 @@
+package hse.kpo.report;
+
+import hse.kpo.domains.Customer;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class ReportBuilder {
+
+    private final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    private final StringBuilder content = new StringBuilder();
+
+    public ReportBuilder addCustomers(List<Customer> customers) {
+        content.append("Покупатели:");
+        customers.forEach(customer -> content.append(String.format(" - %s", customer)));
+        content.append("\n");
+
+        return this;
+    }
+
+    public ReportBuilder addOperation(String operation) {
+        content.append(String.format("Операция: %s", operation));
+        content.append(System.lineSeparator());
+        return this;
+    }
+
+    public Report build() {
+        return new Report(String.format("Отчет за %s", ZonedDateTime.now().format(DATE_TIME_FORMATTER)),
+                content.toString());
+    }
+}
